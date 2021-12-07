01accda76980505f2ac2d6b8012cc2a79a688730
Charlie H <charltonhua@gmail.com>
2021-12-07 00:51:04 -0800

    update displayCurrentWeather function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,12 @@
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city-name-input");
 var displayCityEl = document.querySelector("#city-name");
+var weatherIconEl = document.querySelector("#wicon");
 var displayCityTemperatureEl = document.querySelector("#temperature");
 var displayCityHumidityEl = document.querySelector("#humidity");
 var displayCityWindSpeedEl = document.querySelector("#wind-speed");
+var todaysDate = dayjs().format(`MMM Do, YYYY`);
+// console.log(todaysDate);
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
 
@@ -48,11 +51,16 @@ function displayCityWeather(data) {
   var cityCurrentHumidity = data.main.humidity;
   var cityCurrentTemp = data.main.temp;
   var cityCurrentWindSpeed = data.wind.speed;
+  var icon = JSON.stringify(data.weather[0].icon);
+  icon = icon.replace('"', "");
+  icon = icon.replace('"', "");
+  var weatherIconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-  displayCityEl.textContent = `${city}`;
-  displayCityTemperatureEl.textContent = `Temp: ${cityCurrentTemp}`;
-  displayCityHumidityEl.textContent = `Humidity: ${cityCurrentHumidity}`;
-  displayCityWindSpeedEl.textContent = `Wind Speed: ${cityCurrentWindSpeed}`;
+  displayCityEl.textContent = `${city} (${todaysDate})`;
+  weatherIconEl.setAttribute("src", weatherIconURL);
+  displayCityTemperatureEl.textContent = `Temp: ${cityCurrentTemp} °F`;
+  displayCityHumidityEl.textContent = `Humidity: ${cityCurrentHumidity} %`;
+  displayCityWindSpeedEl.textContent = `Wind Speed: ${cityCurrentWindSpeed} MPH`;
 
   console.log(`${city}
   ${cityLong}
@@ -60,14 +68,15 @@ function displayCityWeather(data) {
   ${cityCurrentHumidity}
   ${cityCurrentTemp}
   ${cityCurrentWindSpeed}
+  ${icon}
   `);
 
   getCityForecast(city);
 }
 
-function getCityForecast(city) {
+function getCityForecast(cityLat, cityLong) {
   var apiKey = "7ee8bad647ef3b941781af45cc24e7a9";
-  var apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  var apiURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&appid=${apiKey}&units=imperial`;
 
   fetch(apiURL).then(function (response) {
     if (response.ok) {
